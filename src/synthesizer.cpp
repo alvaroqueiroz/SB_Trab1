f823721934601bde0d0a68396ec58112e3c17306
Andre <andre.almeida3@gmail.com>
2018-05-17 08:37:02 -0300

    initial synthesizer structure

---
+++ src/synthesizer.cpp
@@ -0,0 +1,9 @@
+#include "synthesizer.h"
+using namespace std;
+
+
+int synthesizer (){
+
+
+    return 0;
+}
---
+++ src/synthesizer.h
@@ -0,0 +1,12 @@
+#ifndef SYNTHESIZER_H_
+#define SYNTHESIZER_H_
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+int synthesizer ();
+
+#endif /* SYNTHESIZER_H__ */
